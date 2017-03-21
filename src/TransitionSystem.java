daf198ac38aa23378026473a6736710e7396efb0
Stephan Matos <Stephanmatos@gmail.com>
2017-03-21 20:07:18 +0100

    Lavet constructor

--- src/TransitionSystem.java
+++ src/TransitionSystem.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
  * Created by Matos on 21-03-2017.
  */
 public class TransitionSystem {
-	
+
 	public static void main(String[]args){
 
-
+        new TransitionSystem();
 
 
 	}
@@ -16,6 +16,10 @@ public class TransitionSystem {
 
         ArrayList<State> TransistionsSystem = new ArrayList<>();
 
+        TransistionsSystem.add(new State(1,true, new String[] {"v"}, new int[] {2}));
+        TransistionsSystem.add(new State(2,false, new String[] {"v"}, new int[] {1,4}));
+        TransistionsSystem.add(new State(3,false, new String[] {"c"}, new int[] {3}));
+        TransistionsSystem.add(new State(4,false, new String[] {"c"}, new int[] {4}));
 
 
     }
