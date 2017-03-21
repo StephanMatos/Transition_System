7915d8e8926466861199daed0a4793e099529a1d
Stephan Matos <Stephanmatos@gmail.com>
2017-03-21 19:36:51 +0100

    hej

--- src/TransitionSystem.java
+++ src/TransitionSystem.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
+
 /**
  * Created by Matos on 21-03-2017.
  */
 public class TransitionSystem {
 
 
+    public TransitionSystem(){
 
-public TransitionSystem(){
+        ArrayList<State> TransistionsSystem = new ArrayList<>();
+
+
+
+    }
+
+public static void main(String[]args){
 
 
 
 
 }
+
 
 
 }
