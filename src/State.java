4b862c3e9d23c4e84ac3ed9d9e02240710beafee
martinthomsenclement <martinthomsenclement@52d3caa2.dynamic-ip.k-net.dk>
2017-03-26 19:33:55 +0200

    Removed whitespace

--- src/State.java
+++ src/State.java
@@ -1,6 +1,3 @@
-/**
- * Created by Matos on 21-03-2017.
- */
 public class State {
 
 public int number;
@@ -9,15 +6,9 @@ public String[] strings;
 public int[] integerArray;
 
     public State(int number,boolean initial, String[] strings, int[] integerArray){
-
-
         this.number = number;
         this.initial = initial;
         this.strings = strings;
         this.integerArray = integerArray;
-
-
     }
-
-
 }
